a38661a246c7bf18dbc0dbdc3ae1eb03c6d4be4b
John Krauss <irving.krauss@gmail.com>
2014-06-22 11:53:08 -0400

    updating database

---
+++ out.sql
@@ -0,0 +1,18 @@
+CREATE TABLE IF NOT EXISTS `out` (
+	`agency_id`	INTEGER,
+	`agency_name`	TEXT,
+	`responsibility_center_id`	INTEGER,
+	`responsibility_center_name`	TEXT,
+	`budget_code_id`	TEXT,
+	`budget_code_name`	TEXT,
+	`object_class`	TEXT,
+	`ic_ref`	INTEGER,
+	`obj`	TEXT,
+	`description`	TEXT,
+	`budget_period`	TEXT,
+	`inc/dec`	TEXT,
+	`key`	TEXT,
+	`value`	INTEGER,
+	`file_name`	TEXT,
+	`source_line`	INTEGER
+);

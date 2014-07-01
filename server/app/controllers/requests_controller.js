66a9d2c7b167d118771fd5a2a5d9fdfa9eb70ff5
Chris Whong <chris.whong@socrata.com>
2014-06-30 22:53:50 -0400

    Added responsibility center summary

---
+++ server/app/controllers/requests_controller.js
@@ -0,0 +1,67 @@
+var _ = require("underscore");
+var sqlite3 = require("sqlite3");
+
+module.exports = function(app) {
+  return app.RequestsController = (function() {
+    function RequestsController() {}
+
+    RequestsController.citySummary = function(req, res) {
+      var db = new sqlite3.Database('../data/processed/all.db');
+      var summary = [];
+      // BAD BAD BAD, we should use real SQL params.  But this is just a demo.
+      var year = Number(req.params.year) - 2000;
+    
+      var statement = "select agency_id, agency_name, sum(value) " +
+                      "from `all` " +
+                      "where " +
+                      "budget_period = 'EXECUTIVE BUDGET FY" + year + "' and " +
+                      "`inc/dec` is null and " +
+                      "key = 'AMOUNT' " +
+                      "group by agency_name " +
+                      "order by sum(value) DESC ";
+      console.log(statement);
+      db.serialize(function() {
+        db.each(statement, function(err, result) {
+          if (err) { console.log(err); }
+          result.more = 'http://localhost:3000/v1/2014/op/' + result.agency_id + '/summary.json'
+          summary.push(result);
+        }, function(){
+          var obj = summary;
+          return app.helpers.output(obj, res, req.params.format);
+        });
+      });
+      
+    };
+
+    RequestsController.agencySummary = function(req, res) {
+      var db = new sqlite3.Database('../data/processed/all.db');
+      var summary = [];
+      // BAD BAD BAD, we should use real SQL params.  But this is just a demo.
+      var year = Number(req.params.year) - 2000;
+    
+      var statement = "select  responsibility_center_name, responsibility_center_name, sum(value) " +
+                      "from `all` " +
+                      "where " +
+                      "budget_period = 'EXECUTIVE BUDGET FY" + year + "' and " +
+                      "`inc/dec` is null and " +
+                      "agency_id = " + req.params.agency + " and " +
+                      "key = 'AMOUNT' " +
+                      "group by responsibility_center_name " +
+                      "order by sum(value) DESC ";
+      console.log(statement);
+      db.serialize(function() {
+        db.each(statement, function(err, result) {
+          if (err) { console.log(err); }
+          summary.push(result);
+        }, function(){
+          var obj = summary;
+          return app.helpers.output(obj, res, req.params.format);
+        });
+      });
+      
+    };
+
+    return RequestsController;
+
+  })();
+};

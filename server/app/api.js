da33d9a01fd7ea701db1f2724dcbe8e26a761923
Volkan Unsal <spocksplanet@gmail.com>
2014-08-09 13:09:09 -0400

    clan up the server folders

--- server/app/api.js
+++ server/app/api.js
@@ -1,16 +1,16 @@
 var _ = require("underscore");
-var sqlite3 = require("sqlite3");
+var sqlite3 = require('sqlite3').verbose();
 
 module.exports = function(app) {
-  return app.RequestsController = (function() {
-    function RequestsController() {}
+  return app.API = (function() {
+    function API() {}
 
-    RequestsController.citySummary = function(req, res) {
+    API.citySummary = function(req, res) {
       var db = new sqlite3.Database('../../data/processed/all.db');
       var summary = [];
       // BAD BAD BAD, we should use real SQL params.  But this is just a demo.
       var year = Number(req.params.year) - 2000;
-    
+
       var statement = "select agency_id, agency_name, sum(value) " +
                       "from `all` " +
                       "where " +
@@ -30,15 +30,15 @@ module.exports = function(app) {
           return app.helpers.output(obj, res, req.params.format);
         });
       });
-      
+
     };
 
-    RequestsController.agencySummary = function(req, res) {
+    API.agencySummary = function(req, res) {
       var db = new sqlite3.Database('../data/processed/all.db');
       var summary = [];
       // BAD BAD BAD, we should use real SQL params.  But this is just a demo.
       var year = Number(req.params.year) - 2000;
-    
+
       var statement = "select  responsibility_center_name, responsibility_center_name, sum(value) " +
                       "from `all` " +
                       "where " +
@@ -58,10 +58,10 @@ module.exports = function(app) {
           return app.helpers.output(obj, res, req.params.format);
         });
       });
-      
+
     };
 
-    return RequestsController;
+    return API;
 
   })();
 };

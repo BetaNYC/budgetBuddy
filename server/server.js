6c89af41de058da14c7103bd7a770f92d1f2ede8
Chris Whong <chris.whong@socrata.com>
2014-08-09 17:58:32 -0400

    Preliminary frontend work with D3

--- server/server.js
+++ server/server.js
@@ -49,6 +49,7 @@ app.configure(function() {
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(app.router);
+  app.use(express.static(__dirname + '/public'));
   return configureSwagger(app.currentEnv);
 });
 

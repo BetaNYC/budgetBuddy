e631c154f89c06d7d4a341dbadf6b8e37359292c
Chris Whong <chris.whong@socrata.com>
2014-08-14 12:01:07 -0400

    Added UoA summary api call

--- server/app/api.js
+++ server/app/api.js
@@ -44,14 +44,42 @@ module.exports = function(app) {
       // BAD BAD BAD, we should use real SQL params.  But this is just a demo.
       var year = Number(req.params.year) - 2000;
 
-      var statement = "select  responsibility_center_name, responsibility_center_name, sum(value) " +
+      var statement = "select  unit_of_appropriation_name, unit_of_appropriation_id, sum(value) " +
                       "from `alladopted` " +
                       "where " +
                       "budget_period = 'ADOPTED BUDGET FY" + year + "' and " +
                       "`inc/dec` is null and " +
                       "agency_id = " + req.params.agency + " and " +
                       "key = 'AMOUNT' " +
-                      "group by responsibility_center_name " +
+                      "group by unit_of_appropriation_name " +
+                      "order by sum(value) DESC ";
+      console.log(statement);
+      db.serialize(function() {
+        db.each(statement, function(err, result) {
+          if (err) { console.log(err); }
+          summary.push(result);
+        }, function(){
+          var obj = summary;
+          return app.helpers.output(obj, res, req.params.format);
+        });
+      });
+
+    };
+
+     API.uoaSummary = function(req, res) {
+      var summary = [];
+      // BAD BAD BAD, we should use real SQL params.  But this is just a demo.
+      var year = Number(req.params.year) - 2000;
+
+      var statement = "select  responsibility_center_name, responsibility_center_id, sum(value) " +
+                      "from `alladopted` " +
+                      "where " +
+                      "budget_period = 'ADOPTED BUDGET FY" + year + "' and " +
+                      "`inc/dec` is null and " +
+                      "agency_id = " + req.params.agency + " and " +
+                      "unit_of_appropriation_id = " + req.params.unitOfAppropriation + " and " +
+                      "key = 'AMOUNT' " +
+                      "group by unit_of_appropriation_name " +
                       "order by sum(value) DESC ";
       console.log(statement);
       db.serialize(function() {

aa30d1df6241b956b8e3057d4a4c312993610834
Chris Whong <chris.whong@socrata.com>
2014-08-14 12:01:07 -0400

    Added UoA summary api call

--- server/app/routes.js
+++ server/app/routes.js
@@ -2,5 +2,6 @@ module.exports = function(app) {
 
   app.get('/v1/:year/op/summary.:format', app.API.citySummary);
   app.get('/v1/:year/op/:agency/summary.:format', app.API.agencySummary);
+  app.get('/v1/:year/op/:agency/:unitOfAppropriation/summary.:format', app.API.uoaSummary);
 
 }

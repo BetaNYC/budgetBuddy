033f25ffb2dc0ba7e442e3c9d22dda1ed6a113aa
Volkan Unsal <spocksplanet@gmail.com>
2014-08-16 19:58:11 -0400

    get rid of the app directory and reunite all endpoints in server.js

--- server/app/routes.js
+++
@@ -1,7 +0,0 @@
-module.exports = function(app) {
-
-  app.get('/v1/:year/op/summary.:format', app.API.citySummary);
-  app.get('/v1/:year/op/:agency/summary.:format', app.API.agencySummary);
-  app.get('/v1/:year/op/:agency/:unitOfAppropriation/summary.:format', app.API.uoaSummary);
-
-}
